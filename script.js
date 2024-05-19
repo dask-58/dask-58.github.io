9af021fdf657becea43863b2782a98347d2c4877
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-05-19 19:17:32 +0530

    Add Feature: Light and Dark Mode

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log("Did you come to inspect my website ? ")
 console.log("Go ahead!")
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('light-mode');
+});
