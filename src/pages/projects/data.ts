fa336922f8cce0cb306e944a8b99cb115f3b6831
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-07-09 20:26:21 +0530

    initial commit

---
+++ src/pages/projects/data.ts
@@ -0,0 +1,32 @@
+import type { ProjectData } from '@/types'
+
+export const projectData: ProjectData = [
+  {
+    title: '2024',
+    projects: [
+      {
+        text: 'Erudite',
+        description: 'Ai chat bot built with Google Gemini API.',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+      {
+        text: '[ . ] Files',
+        description: 'My dot files',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: '2023',
+    projects: [
+      {
+        text: 'Spend Elon Money',
+        description: 'Simulate spending the money of the richest man on Earth; as you wish!',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+    ],
+  },
+]
---
+++ src/site-config.ts
@@ -0,0 +1,77 @@
+export const siteConfig = {
+  author: 'Dhruv',
+  title: 'Dhruv Koli | Home',
+  subtitle: 'Competitive Programmer | IIIT Dharwad',
+  description: 'Dhruv\'s personal webpage.',
+  image: {
+    src: '/hero.jpg',
+    alt: 'Website Main Image',
+  },
+  email: 'googldhruv@gmail.com',
+  socialLinks: [
+    {
+      text: 'GitHub',
+      href: 'https://github.com/dask-58',
+      icon: 'i-simple-icons-github',
+      header: 'i-ri-github-line',
+    },
+    {
+      text: 'Twitter',
+      href: 'https://x.com/dask_58',
+      icon: 'i-simple-icons-x',
+      header: 'i-ri-twitter-x-line',
+    },
+    {
+      text: 'Linkedin',
+      href: 'https://linkedin.com/in/dhruvkoli',
+      icon: 'i-simple-icons-linkedin',
+    },
+    {
+      text: 'Instagram',
+      href: 'https://instagram.com/dask_58',
+      icon: 'i-simple-icons-instagram',
+    },
+  ],
+  header: {
+    logo: {
+      src: '/logonew.gif',
+      alt: 'Logo Image',
+    },
+    navLinks: [
+      {
+        text: 'Home',
+        href: 'https://dask-58.github.io',
+      },
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Projects',
+        href: '/projects',
+      },
+    ],
+  },
+  page: {
+    blogLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+    ],
+  },
+  footer: {
+    navLinks: [
+      {
+        text: 'Posts Props',
+        href: '/posts-props',
+      },
+      {
+        text: 'GitHub Repository',
+        href: 'https://github.com/dask-58/dask-58.github.io',
+      },
+    ],
+  },
+}
+
+export default siteConfig
