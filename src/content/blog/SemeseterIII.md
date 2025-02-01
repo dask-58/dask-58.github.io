c183b2ac3edeac878d45a65df20e06da95a0761f
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-02-01 15:52:05 +0530

    rm -rf *

--- src/content/blog/SemeseterIII.md
+++
@@ -1,7 +0,0 @@
----
-title: Almost Failed Sem III
-description: Semester III overview
-date: 2024-04-12
-tag: ICPC, College
-duration: 4 min
----
--- src/utils/link.ts
+++
@@ -1,7 +0,0 @@
-export function getLinkTarget(link: string) {
-  return link.includes('https') ? '_blank' : '_self'
-}
-
-export function isExternalLink(link: string) {
-  return link.includes('https')
-}
